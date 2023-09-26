122be2834b430eaa5c52b7b342eb80757a4e37a7
Vinicius José <vinicius840_js@hotmail.com>
2023-09-26 15:18:07 -0300

    start of menu creation

---
+++ components/menu/Menu.tsx
@@ -0,0 +1,17 @@
+export default function Menu(){
+  return(
+    <header>
+      <nav>
+        <a href="">
+          logo
+        </a>
+        <ul>
+          <li>serviços</li>
+          <li>projetos</li>
+          <li>sobre min</li>
+          <li>contato</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
