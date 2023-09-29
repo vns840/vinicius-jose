ee3d0496bc9cc69501b92b22c9aee7b158f30794
Vinicius José <vinicius840_js@hotmail.com>
2023-09-29 16:47:59 -0300

    construction of the service section

--- components/servicos/Servicos.tsx
+++ components/servicos/Servicos.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+
 export default function Servicos(){
   return(
-    <section id="servicos">
-      <h2>Serviços</h2>
+    <section className="" id="servicos">
+      <h2 className="text-center">Serviços</h2>
+     <div className="flex items-center justify-center">
+     <div className="text-left justify-start">
+        <h3>serviços oferecidos</h3>
+        <ul>
+          <li>web site</li>
+          <li>landing page</li>
+          <li>email marketing</li>
+          <li>blog - wordpress</li>
+          <li>manutenção de site</li>
+        </ul>
+      </div>
+      <div className="justify-end">
+        <Image src="/images/notes-services.svg" width={500} height={500} alt="" />
+      </div>
+     </div>
     </section>
   )
 }
