455f6abcaebd394f7b70d6c5bcdb995caaeae072
Vinicius José <vinicius840_js@hotmail.com>
2023-09-29 10:19:03 -0300

    adjustments in the hero section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 export default function Hero(){
   return(
-    <section id="hero"> 
-      <h1>Vinicius José</h1>
-      <p>Desenvolverdor Front end</p>
+    <section className="flex items-center justify-center" id="hero"> 
+     <div className="justify-start w-[50%]">
+      <Image src="/images/programming-hero.svg" width={500} height={500} alt="" />
+     </div>
+     <div className="text-center justify-end">
+      <h1 className="font-bold text-5xl	mt-2">Vinicius José</h1>
+      <p className="font-light text-xl">Desenvolverdor Front end <span>{` < /> `}</span></p>
+     </div>
     </section>
   )
 }
