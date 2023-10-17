b10ce6edd4d020a165a94e771afd3a3c1a3a0e7f
vinicius josé <vinicius840_js@hotmail.com>
2023-10-17 11:31:01 -0300

    test image

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero(){
   return(
     <section className="flex items-center justify-center max-md:flex-col max-md:h-auto" id="hero"> 
      <div className="justify-start w-[50%]">
-      <Image src="/images/programming-hero.svg" width={500} height={500} alt="" />
+      <Image src="../images/programming-hero.svg" width={500} height={500} alt="" />
      </div>
      <div className="text-center justify-end">
       <h1 className="font-bold text-5xl	mt-2 text-purple01">Vinicius José</h1>
