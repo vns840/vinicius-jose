967da129822bf7284e58af9125d96ca672b201b1
vinicius josé <vinicius840_js@hotmail.com>
2023-10-18 17:03:47 -0300

    addition of the head component

---
+++ components/Head/Head.tsx
@@ -0,0 +1,8 @@
+export function Head(){
+  return(
+    <>
+      <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+      <title>Vinicius José | Dev Front-end</title>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+
+import { Head } from "@/components/Head/Head";
 import Hero from "@/components/Hero/Hero";
 import Contato from "@/components/contato/Contato";
 import Footer from "@/components/footer/Footer";
@@ -6,9 +8,11 @@ import Projetos from "@/components/projetos/Projetos";
 import Servicos from "@/components/servicos/Servicos";
 import SobreMIn from "@/components/sobreMin/SobreMIn";
 
+
 export default function Home() {
   return (
    <>
+   <Head />
    <Menu />
     <Hero />
     <Servicos />
